6fa9feaadf9e0b63e0b77073d49810b364cc418c
x-finity <x.finity777@gmail.com>
2024-10-10 22:08:06 +0300

    add dotenv package

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +79,6 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-import os
 
 DATABASES = {
     'default': {
